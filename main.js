642c22a4a5ff937704093b15c09391d77a5e2ab2
marcdledda <marcdl636@gmail.com>
2018-01-26 11:53:07 -0600

    added stuff

--- main.js
+++ main.js
@@ -7,12 +7,16 @@ let smallSample = [1,2,3];
 // console.log(smallSquare);
 
 
-let testy = [234];
-let sTesty = testy.toString().split('');
+let testy = [19];
+let output = [];
 
 for (i = 0; i < testy.length; i++) {
-    let splitting = testy.toString().split('');
-    let mathSplit = parseInt(splitting);
-    console.log(mathSplit);
+    let splitting = testy[i].toString().split('');
+    console.log(splitting);
+    output.push(splitting);
 }
-
+console.log(output);
+for (i = 0; i < output.length; i++) {
+    let PLS = parseInt(output);
+    console.log(PLS);
+}

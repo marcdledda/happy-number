431b5aa4d9a13e976bbd49822c9a1484418d6269
marcdledda <marcdl636@gmail.com>
2018-01-26 10:46:30 -0600

    stuff is hard

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("JS TEST");
+
+
+
+let smallSample = [1,2,3];
+// let smallSquare = math.square(smallSample);
+// console.log(smallSquare);
+
+
+let testy = [234];
+let sTesty = testy.toString().split('');
+
+for (i = 0; i < testy.length; i++) {
+    let splitting = testy.toString().split('');
+    let mathSplit = parseInt(splitting);
+    console.log(mathSplit);
+}
+
